461b6e0c5851d59af821e4c7c8eb0282e2e8a987
moazzam ali qazi <92213316+moazzamaliqazi1@users.noreply.github.com>
2022-08-09 20:58:01 -0700

    EDITING

--- client/src/components/shared/Reviewbanner.jsx
+++ client/src/components/shared/Reviewbanner.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
-import revbannerImage from "../../images/rev-ban.jpg"
-const ReviewBanner = ()=>{
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import rating from "../../images/rating.png"
+
+const ReviewBanner = () => {
     return (
         <>
-            <div>
-                <div className="container-fluid col-md-12 my-3">
-                    <img src={revbannerImage} alt="" style={{ width: '100%' }} />
-                </div>
+            <div className='m-5'>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[Autoplay]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <center>
+                            <div className='container' style={{ color: '#002856' }}>
+                                <div className='row'>
+                                    <div className='col-md-12 mt-2'>
+                                        <img src={rating} style={{ width: '200px', height: '150px' }} />
+                                        <h3 className='fw-bold mt-2'>UMAR NASIR</h3>
+                                        <h4>Great quality for the price!</h4>
+                                        <h6>"I was so impressed with the quality of the prints as well as the framing. <br />My photos came within the same week of my order in perfect condition with hanging pieces. <br />Love and will use again!"</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <center>
+                            <div className='container' style={{ color: '#002856' }}>
+                                <div className='row'>
+                                    <div className='col-md-12 mt-2'>
+                                        <img src={rating} style={{ width: '200px', height: '150px' }} />
+                                        <h3 className='fw-bold mt-2'>SYED YASIR ABBASS</h3>
+                                        <h4>Beautiful!</h4>
+                                        <h6>"I got this as a gift for a friend and the quality was great and <br />it turned out beuaitful! Highly recommend!"</h6>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </center>
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </>
     )
